31cdc42baa524984c2386c6d6e4ec6e35ed615d8
Anamiikeya <47068601+Anamikeya@users.noreply.github.com>
2021-03-05 21:38:11 +0530

    first commit
    No need

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() 
+{ 
+    char c = 'r';
+    char d = 'R'; 
+  
+    // %d displays the integer value of a character 
+    // %c displays the actual character 
+    printf("The ASCII value of %c is %d\n", c, c); 
+    printf("The ASCII value of %c is %d\n", d, d); 
+    
+    return 0; 
+} 
